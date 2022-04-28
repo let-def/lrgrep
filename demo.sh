588638ab6a0ded0ef5dd7a0802956509eb24c967
Frédéric Bour <fred@tarides.com>
2022-04-28 11:29:03 +0900

    demo script

---
+++ demo.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+eval `./setup_shell.sh`
+for i in test_ko_01.ml test_ko_03.ml test_ko_04.ml test_ko_05.ml test_ko_06.ml; do
+  printf "\033c"
+  echo '$' cat "$i"
+  source-highlight -f esc256 -i "$i" -o /dev/fd/1 | term-invert-colors
+  read
+
+  echo -n '$' ocamlc -c "$i"
+  read
+  ocamlc -c "$i"
+  read
+done
