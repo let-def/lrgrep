8cb50ee9cbe9938dce7e072c8063b3a2584ef043
Frédéric Bour <fred@tarides.com>
2022-10-14 17:15:50 +0200

    Make setup_shell.sh more robust (handle sourcing or evaling, handle relative paths from bash or zsh)

--- demo/setup_shell.sh
+++ demo/setup_shell.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-echo export "'OCAMLPARAM=pp=$PWD/../_build/default/ocaml/frontend.bc,_'"
+
+# Directly export in case setup_shell.sh
+if [ -r "${BASH_SOURCE[0]:-${(%):-%x}}" ]; then
+  export OCAMLPARAM="pp=$(realpath $(dirname ${BASH_SOURCE[0]:-${(%):-%x}})/../_build/default/ocaml/frontend.bc,_)"
+else
+  export OCAMLPARAM="pp=$(realpath $(dirname $0/../_build/default/ocaml/frontend.bc,_))"
+fi
+echo export "OCAMLPARAM='$OCAMLPARAM'"
