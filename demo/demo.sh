77a9321ff97fe9e0b6dbd52a1cffd28bac5ecff6
Frédéric Bour <frederic.bour@lakaban.net>
2023-01-09 18:27:48 +0900

    Fix demo script
    It broke with zsh and if source-highlight was not installed.

--- demo/demo.sh
+++ demo/demo.sh
@@ -1,12 +1,38 @@
 #!/bin/sh
+
+if which term-invert-colors; then
+	invert()
+	{
+		term-invert-colors
+	}
+else
+	invert()
+	{
+		cat -
+	}
+fi
+
+if which source-highlight; then
+	hl()
+	{
+		source-highlight -f esc256 -i "$1" -o /dev/fd/1
+	}
+else
+	hl()
+	{
+		cat "$1"
+	}
+fi
+
 eval `./setup_shell.sh`
 for i in test_ko_01.ml test_ko_02.ml test_ko_03.ml test_ko_04.ml test_ko_05.ml test_ko_06.ml test_ko_07.ml test_ko_08.ml; do
   printf "\033c"
-  echo '$' cat "$i"
-  source-highlight -f esc256 -i "$i" -o /dev/fd/1 | term-invert-colors
+  printf "$ cat $i\n"
+  read
+  hl "$i" | invert
   read
 
-  echo -n '$' ocamlc -c "$i"
+  printf "$ ocamlc -c $i"
   read
   ocamlc -c "$i"
   read
--- demo/setup_shell.sh
+++ demo/setup_shell.sh
@@ -2,8 +2,8 @@
 
 # Directly export in case setup_shell.sh
 if [ -r "${BASH_SOURCE[0]:-${(%):-%x}}" ]; then
-  export OCAMLPARAM="pp=$(realpath $(dirname ${BASH_SOURCE[0]:-${(%):-%x}})/../_build/default/ocaml/frontend.bc,_)"
+  export OCAMLPARAM="pp=$(realpath $(dirname ${BASH_SOURCE[0]:-${(%):-%x}})/../_build/default/ocaml/frontend.bc),_"
 else
-  export OCAMLPARAM="pp=$(realpath $(dirname $0/../_build/default/ocaml/frontend.bc,_))"
+  export OCAMLPARAM="pp=$(realpath $(dirname $0/../_build/default/ocaml/frontend.bc)),_"
 fi
 echo export "OCAMLPARAM='$OCAMLPARAM'"
