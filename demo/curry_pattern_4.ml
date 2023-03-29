92c042fe32397888c227bddbca9d7ab2be8c8859
Frédéric Bour <frederic.bour@lakaban.net>
2023-03-29 20:55:59 +0200

    Prepare for pres

--- demo/curry_pattern_4.ml
+++ demo/curry_pattern_4.ml
@@ -1,1 +1,7 @@
 function Foo Bar Baz _ _ -> ()
+
+(*
+  | inner=constr_longident;
+    [_* / pattern_gen: constr_longident pattern .]
+    @ UNDERSCORE | LIDENT | UIDENT | LPAREN
+*)
--- demo/test_ko_03.ml
+++ demo/test_ko_03.ml
@@ -1,1 +1,6 @@
 let x = (5
+
+(* 
+  | lp=LPAREN; [_* / . RPAREN]
+     { "Unclosed parenthesis at line "
+*)
