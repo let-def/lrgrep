2cb0ebda45b398283aaaf010dbf3851f377d1669
Frédéric Bour <frederic.bour@lakaban.net>
2023-04-13 10:44:24 +0200

    Add IntSetSet

--- src/utils/indexSet.ml
+++ src/utils/indexSet.ml
@@ -9,7 +9,7 @@ module F (X : Index.Unsafe.T) = struct
   end
 end
 
-module IntSet : F(Index.Unsafe.Int).S =
+module FIntSet : F(Index.Unsafe.Int).S =
 struct
   type _element = int
   type _t = IntSet.t
@@ -22,7 +22,9 @@ struct
   let unsafe_to_indexset x = x
 end
 
-include Index.Unsafe.Coerce(F)(IntSet)
+module IndexSet = Index.Unsafe.Coerce(F)(FIntSet)
+
+include IndexSet
 
 module CoerceSum(X : CARDINAL)(Y : CARDINAL) = struct
   let coerce : X.n t -> Sum(X)(Y).n t = unsafe_to_indexset
@@ -32,3 +34,24 @@ let init_from_set c f =
   match Fix.Indexing.cardinal c with
   | 0 -> empty
   | n -> init_subset (Index.of_int c 0) (Index.of_int c (n - 1)) f
+
+
+module FF (X : Index.Unsafe.T) = struct
+  module type S = sig
+    include SetSig.StdSetS1 with type 'a t = IntSetSet.t
+                             and type 'a elt = 'a X.t IndexSet.t
+  end
+end
+
+module FIntSetSet : FF(Index.Unsafe.Int).S =
+struct
+  type _elt = IntSet.t
+  type _t = IntSetSet.t
+
+  type 'a elt = _elt
+  type 'a t = _t
+
+  include (IntSetSet: Set.S with type elt := _elt and type t := _t)
+end
+
+module Set = Index.Unsafe.Coerce(FF)(FIntSetSet)
