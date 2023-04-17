845b879f634b21f35c7371fdb2c17c63e064d0af
Frédéric Bour <frederic.bour@lakaban.net>
2023-04-17 12:16:36 +0200

    Add IntSetMap and generic IndexSetMap

--- src/utils/indexSet.ml
+++ src/utils/indexSet.ml
@@ -1,6 +1,6 @@
 open Fix.Indexing
 
-module F (X : Index.Unsafe.T) = struct
+module FSet (X : Index.Unsafe.T) = struct
   module type S = sig
     include SetSig.S1 with type 'a t = IntSet.t
                        and type 'a element = 'a X.t
@@ -9,7 +9,7 @@ module F (X : Index.Unsafe.T) = struct
   end
 end
 
-module FIntSet : F(Index.Unsafe.Int).S =
+module FIntSet : FSet(Index.Unsafe.Int).S =
 struct
   type _element = int
   type _t = IntSet.t
@@ -22,7 +22,7 @@ struct
   let unsafe_to_indexset x = x
 end
 
-module IndexSet = Index.Unsafe.Coerce(F)(FIntSet)
+module IndexSet = Index.Unsafe.Coerce(FSet)(FIntSet)
 
 include IndexSet
 
@@ -36,14 +36,14 @@ let init_from_set c f =
   | n -> init_subset (Index.of_int c 0) (Index.of_int c (n - 1)) f
 
 
-module FF (X : Index.Unsafe.T) = struct
+module FSetSet (X : Index.Unsafe.T) = struct
   module type S = sig
     include SetSig.StdSetS1 with type 'a t = IntSetSet.t
                              and type 'a elt = 'a X.t IndexSet.t
   end
 end
 
-module FIntSetSet : FF(Index.Unsafe.Int).S =
+module FIntSetSet : FSetSet(Index.Unsafe.Int).S =
 struct
   type _elt = IntSet.t
   type _t = IntSetSet.t
@@ -54,4 +54,24 @@ struct
   include (IntSetSet: Set.S with type elt := _elt and type t := _t)
 end
 
-module Set = Index.Unsafe.Coerce(FF)(FIntSetSet)
+module Set = Index.Unsafe.Coerce(FSetSet)(FIntSetSet)
+
+module FSetMap (X : Index.Unsafe.T) = struct
+  module type S = sig
+    include SetSig.StdMapS1 with type ('n, 'a) t = 'a IntSetMap.t
+                             and type 'n key = 'n X.t IndexSet.t
+  end
+end
+
+module FIntSetMap : FSetMap(Index.Unsafe.Int).S =
+struct
+  type _key = IntSet.t
+  type 'a _t = 'a IntSetMap.t
+
+  type 'n key = _key
+  type ('n, 'a) t = 'a _t
+
+  include (IntSetMap: Map.S with type key := _key and type 'a t := 'a _t)
+end
+
+module Map = Index.Unsafe.Coerce(FSetMap)(FIntSetMap)
--- src/utils/indexSet.mli
+++ src/utils/indexSet.mli
@@ -11,3 +11,6 @@ val init_from_set : 'a cardinal -> ('a index -> bool) -> 'a t
 
 module Set : SetSig.StdSetS1 with type 'a t = private IntSetSet.t
                               and type 'a elt = 'a t
+
+module Map : SetSig.StdMapS1 with type ('n, 'a) t = private 'a IntSetMap.t
+                              and type 'n key = 'n t
