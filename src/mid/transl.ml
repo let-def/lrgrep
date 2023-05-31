e3a214c646cdaefee96fcb67cacb24dda5c0f317
Frédéric Bour <frederic.bour@lakaban.net>
2023-05-31 18:19:19 +0900

    Fix item globbing

--- src/mid/transl.ml
+++ src/mid/transl.ml
@@ -221,6 +221,7 @@ struct
     and glob_exact = {
       dots: IntSet.t;
       syms: Symbol.set array;
+      length: int;
       skip: glob_skip option;
     }
 
@@ -236,8 +237,7 @@ struct
       | [] -> ([], [])
       | (Skip, _pos) :: rest ->
         let last, tail = structure_filter rest in
-        let dots, syms = parse_component last in
-        ([], {dots; syms; skip=None} :: tail)
+        ([], parse_component last :: tail)
       | (Dot, _pos) :: rest ->
         let last, tail = structure_filter rest in
         (`Dot :: last, tail)
@@ -249,261 +249,127 @@ struct
         (`Find set :: last, tail)
 
     let normalize_filter = function
-      | [] -> {dots=IntSet.empty; syms=[||]; skip=None}
-      | [exact] -> exact
-      | prefix :: rest ->
+      | [] -> {dots=IntSet.empty; syms=[||]; skip=None; length=0}
+      | [dots, syms] -> {dots; syms; skip=None; length = Array.length syms}
+      | (dots, syms) :: rest ->
         let rec loop = function
           | [] -> assert false
-          | [exact] ->
-            {dot = false; exact}
-          | {dots; syms=[||]; _} :: xs ->
+          | [dots, syms] ->
+            {dot = false; exact={dots; syms; skip=None; length=Array.length syms}}
+          | (dots, [||]) :: xs ->
             if IntSet.is_empty dots
             then loop xs
             else {(loop xs) with dot = true}
-          | exact :: rest ->
-            {dot = false; exact = {exact with skip = Some (loop rest)}}
+          | (dots, syms) :: rest ->
+            {dot = false; exact = loop_skip dots syms rest}
+        and loop_skip dots syms rest =
+          let skip = loop rest in
+          let length = Array.length syms + skip.exact.length in
+          {dots; syms; length; skip = Some skip}
         in
-        {prefix with skip = Some (loop rest)}
+        loop_skip dots syms rest
 
-    let sub_match rhs pos syms =
-      let lsyms = Array.length syms in
-      (pos + lsyms <= Array.length rhs) &&
-      let rec loop pos i =
-        (i >= lsyms) || (
-          IndexSet.mem rhs.(pos) syms.(i) &&
-          loop (pos + 1) (i + 1)
+    let parse filter =
+      let last, tail = structure_filter filter in
+      let comp = parse_component last in
+      normalize_filter (comp :: tail)
+
+    let sub_match rhs pos exact =
+      (pos + exact.length <= Array.length rhs) &&
+      let rec loop i =
+        (i = Array.length exact.syms) || (
+          IndexSet.mem rhs.(pos + i) exact.syms.(i) &&
+          loop (i + 1)
         )
       in
-      loop pos 0
+      loop 0
 
-    let rec match_skip rhs pos skip =
-      match skip.exact.skip with
+    let rec match_skip rhs pos {dot=_;exact} =
+      match exact.skip with
       | None ->
         (* Match suffix *)
-        let pos' = Array.length rhs - Array.length skip.exact.syms in
-        (pos' >= pos) && sub_match rhs pos' skip.exact.syms
+        let pos' = Array.length rhs - Array.length exact.syms in
+        (pos' >= pos) && sub_match rhs pos' exact
       | Some skip' ->
         (* Match substring *)
         let rec loop pos =
-          (pos <= Array.length rhs) &&
-          if not (sub_match rhs pos skip.exact.syms)
+          (pos + exact.length <= Array.length rhs) &&
+          if not (sub_match rhs pos exact)
           then loop (pos + 1)
-          else match_skip rhs (pos + Array.length skip.exact.syms) skip'
+          else match_skip rhs (pos + Array.length exact.syms) skip'
         in
         loop pos
 
     let add_dots pos exact dots =
       IntSet.fold (fun i acc -> IntSet.add (pos + i) acc) exact.dots dots
 
-    let rec extract_skip rhs pos skip =
-      match skip.exact.skip with
+    let rec extract_skip rhs pos {dot; exact} =
+      match exact.skip with
       | None ->
         (* Match suffix *)
-        let pos' = Array.length rhs - Array.length skip.exact.syms in
-        if (pos' >= pos) && sub_match rhs pos' skip.exact.syms then
+        let pos' = Array.length rhs - exact.length in
+        if (pos' >= pos) && sub_match rhs pos' exact then
           let dots =
-            if skip.dot
+            if dot
             then IntSet.init_interval pos pos'
             else IntSet.empty
           in
-          Some (add_dots pos' skip.exact dots)
+          Some (add_dots pos' exact dots)
         else
           None
 
       | Some skip' ->
         (* Match substring *)
-        let rec loop_match i j =
+        let rec downmatch i j =
           if j <= i then i
-          else if sub_match rhs j skip.exact.syms &&
-                  match_skip rhs (j + Array.length skip.exact.syms) skip' then
+          else if sub_match rhs j exact &&
+                  match_skip rhs (j + Array.length exact.syms) skip' then
             j
           else
-            loop_match i (j - 1)
+            downmatch i (j - 1)
         in
-        let rec loop pos' =
-          if pos' > Array.length rhs then None
-          else if not (sub_match rhs pos' skip.exact.syms)
-          then loop (pos' + 1)
+        let rec upmatch pos =
+          if pos + exact.length > Array.length rhs then None
+          else if not (sub_match rhs pos exact)
+          then upmatch (pos + 1)
           else
-            match extract_skip rhs (pos' + Array.length skip.exact.syms) skip' with
+            match extract_skip rhs (pos + Array.length exact.syms) skip' with
             | None -> None
             | Some dots ->
+              let dots = add_dots pos exact dots in
               let dots =
-                if skip.dot then
+                if dot then
                   IntSet.union dots
-                    (IntSet.init_interval pos' (loop_match pos' (Array.length rhs - 1)))
+                    (IntSet.init_interval pos
+                       (downmatch pos (Array.length rhs - skip'.exact.length)))
                 else
                   dots
               in
               Some dots
         in
-        loop pos
-
-    let match_exact rhs pos exact =
-      sub_match rhs pos exact.syms &&
-      let pos' = pos + Array.length exact.syms in
-      match exact.skip with
-      | None -> Array.length rhs = pos'
-      | Some skip -> match_skip rhs pos' skip
+        upmatch pos
 
     let extract rhs exact =
-      if not (sub_match rhs 0 exact.syms) then
-        None
+      if not (sub_match rhs 0 exact) then
+        IntSet.empty
       else
         let pos' = Array.length exact.syms in
         match exact.skip with
         | None ->
           if Array.length rhs = pos'
-          then Some exact.dots
-          else None
+          then exact.dots
+          else IntSet.empty
         | Some skip ->
           match extract_skip rhs pos' skip with
-          | None -> None
-          | Some dots -> Some (IntSet.union dots exact.dots)
+          | None ->
+            assert (not (match_skip rhs pos' skip));
+            IntSet.empty
+          | Some dots -> IntSet.union dots exact.dots
   end
 
   let match_sym sym = function
     | None -> true
     | Some sym' -> Misc.equal_index sym sym'
-
-  type normalized_filter =
-    | NDot of position
-    | NSym of Symbol.set
-
-  let normalize_rhs rhs =
-    let rec norm_dup = function
-      | [] -> []
-      | (Skip, pos1) :: (Skip, pos2) :: rest ->
-        warn pos2 "Redundant '...'";
-        norm_dup ((Skip, pos1) :: rest)
-      | (Dot, pos1) :: (Dot, pos2) :: rest ->
-        warn pos2 "Redundant '.'";
-        norm_dup ((Dot, pos1) :: rest)
-      | (Find str, pos) :: rest ->
-        let sym = Indices.find_symbols str in
-        if IndexSet.is_empty sym then
-          error pos "Unknown symbol %s" (Indices.string_of_symbol (Option.get str));
-        begin match norm_dup rest with
-          | x :: xs -> (NSym sym :: x) :: xs
-          | [] -> [[NSym sym]]
-        end
-      | (Dot, pos1) :: rest ->
-        begin match norm_dup rest with
-          | (NDot pos2 :: x) :: xs ->
-            warn pos2 "Redundant '.'";
-            (NDot pos1 :: x) :: xs
-          | x :: xs ->
-            (NDot pos1 :: x) :: xs
-          | [] ->
-            [[NDot pos1]]
-        end
-      | (Skip, pos1) :: rest ->
-        begin match norm_dup rest with
-          | [] :: _ as result ->
-            warn pos1 "Redundant '...'";
-            result
-          | [NDot pos1] :: [NDot pos2] :: xs ->
-            warn pos2 "Redundant '...'";
-            [NDot pos1] :: xs
-          | [] -> [[]; []]
-          | xs -> [] :: xs
-        end
-    in
-    norm_dup rhs
-
-  let glob_filter rhs filter =
-    let rec match_test rhs pos = function
-      | NDot _ :: seek ->
-        match_test rhs pos seek
-      | NSym syms :: seek ->
-        if (pos < Array.length rhs) && IndexSet.mem rhs.(pos) syms
-        then match_test rhs (pos + 1) seek
-        else None
-      | [] -> Some pos
-    in
-    let rec match_seek rhs pos test =
-      match match_test rhs pos test with
-      | None when pos < Array.length rhs ->
-        match_seek rhs (pos + 1) test
-      | x -> x
-    in
-    let rec test_length = function
-      | NDot _ :: rest -> test_length rest
-      | NSym _ :: rest -> 1 + test_length rest
-      | [] -> 0
-    in
-    let rec match_rest_only rhs pos = function
-      | [] -> pos = Array.length rhs
-      | seek :: rest ->
-        let length = test_length seek in
-        let pos' = Array.length rhs - length in
-        (pos' >= pos) &&
-        match rest with
-        | [] -> length = 0 || Option.is_some (match_test rhs pos' seek)
-        | rest ->
-          match match_seek rhs pos seek with
-          | None -> false
-          | Some pos -> match_rest_only rhs pos rest
-    in
-    let rec place_dots acc pos = function
-      | [] -> acc
-      | NDot _ :: rest -> place_dots (IntSet.add pos acc) pos rest
-      | NSym _ :: rest -> place_dots acc (pos + 1) rest
-    in
-    let place_dots pos test acc = place_dots acc pos test in
-    let rec match_rest_dot rhs pos = function
-      | [] ->
-        if pos = Array.length rhs
-        then Some IntSet.empty
-        else None
-      | [[]] -> Some IntSet.empty
-      | [] :: rest ->
-        match_rest_dot rhs pos rest
-      | [NDot _] :: rest ->
-        begin match match_rest_dot rhs pos rest with
-          | None -> None
-          | Some dots ->
-            let dots = ref (IntSet.add pos dots) in
-            let pos = ref (pos + 1) in
-            while !pos <= Array.length rhs &&
-                  match_rest_only rhs !pos rest
-            do
-              dots := IntSet.add !pos !dots;
-              incr pos
-            done;
-            Some !dots
-        end
-      | seek :: rest ->
-        let length = test_length seek in
-        let pos' = Array.length rhs - length in
-        if pos' < pos then
-          None
-        else (match rest with
-          | [] ->
-            if length = 0 || Option.is_some (match_test rhs pos' seek)
-            then Some (place_dots pos seek IntSet.empty)
-            else None
-          | rest ->
-            match match_seek rhs pos seek with
-            | None -> None
-            | Some pos ->
-              Option.map (place_dots (pos - length) seek)
-                (match_rest_dot rhs pos rest)
-          )
-    in
-    let match_dot rhs = function
-      | [test] ->
-        let length = test_length test in
-        if Array.length rhs = length then
-          Some (place_dots 0 test IntSet.empty)
-        else
-          None
-      | [] :: rest -> match_rest_dot rhs 0 rest
-      | prefix :: rest ->
-        Option.map (place_dots 0 prefix) (match_rest_dot rhs 0 rest)
-      | [] -> None
-    in
-    Option.value (match_dot rhs filter) ~default:IntSet.empty
 
   let transl_filter position ~lhs ~rhs =
     let transl_sym = Option.map (Indices.get_symbol position) in
@@ -515,10 +381,10 @@ struct
         | L _ -> error position "left-handside of a filter should be a non-terminal"
         | R n -> Indices.prod_by_lhs n
     in
-    let filter = normalize_rhs rhs in
+    let filter = Globbing.parse rhs in
     let matching_dots prod =
-      let result = glob_filter (Production.rhs prod) filter in
-      begin match IntSet.elements result with
+      let result = Globbing.extract (Production.rhs prod) filter in
+      (*begin match IntSet.elements result with
         | [] -> ()
         | dots ->
           let rhs = Array.to_list (Production.rhs prod) in
@@ -526,7 +392,7 @@ struct
             | (pos' :: dots), rhs when pos = pos' ->
               "." :: annotate pos (dots, rhs)
             | [], [] -> []
-            | dots, [] -> "WEIRD @" :: List.map string_of_int dots
+            | dots, [] -> assert (dots = []); []
             | dots, (x :: rhs) ->
               Symbol.name x :: annotate (pos + 1) (dots, rhs)
           in
@@ -534,7 +400,7 @@ struct
           warn position "filter matches %s: %s"
             (Nonterminal.to_string (Production.lhs prod))
             (String.concat " " txt)
-      end;
+        end;*)
       result
     in
     let matching_states prod =
