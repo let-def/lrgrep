b0169e2c9894fedd96b3f000d3c105af9e2cda46
Frédéric Bour <frederic.bour@lakaban.net>
2023-07-11 12:05:39 +0900

    Fancy stopwatch to log time

---
+++ src/utils/stopwatch.mli
@@ -0,0 +1,11 @@
+type t
+
+val create : unit -> t
+
+val reset : t -> unit
+
+val step : t -> ('a, unit, string, unit) format4 -> 'a
+
+val enter : t -> ('a, unit, string, t) format4 -> 'a
+
+val leave : t -> unit
