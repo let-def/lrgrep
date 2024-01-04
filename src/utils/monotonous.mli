9f989f2a2d2e08c8e98453b4806c10b717c330a3
Frédéric Bour <frederic.bour@lakaban.net>
2024-01-04 14:41:33 +0900

    Revert "Wrong attempt at new monotonous impl"
    This reverts commit 4ebbecd98a9afe8356b62985e17c7b1732fd1406.

--- src/utils/monotonous.mli
+++ src/utils/monotonous.mli
@@ -1,16 +1,32 @@
 open Fix.Indexing
 
-type ('a, 'b) piece = ('a IndexSet.t * 'b IndexSet.t)
-type ('a, 'b) t = private ('a, 'b) piece list
+module Increasing : sig
+  type ('a, 'b) piece = ('a IndexSet.t * 'b IndexSet.t)
+  type ('a, 'b) t = private ('a, 'b) piece list
 
-val minimum : ('a, 'b) t
-val is_minimum : ('a, 'b) t -> bool
-val piece : 'a IndexSet.t -> 'b IndexSet.t -> ('a, 'b) t
-val piecewise : ('a IndexSet.t * 'b IndexSet.t) list -> ('a, 'b) t
-val increase :
-  ?ignore:'b IndexSet.t ->
-  ('a, 'b) t -> ('a, 'b) t -> ('a, 'b) t * ('a, 'b) t
-val image : ('a, 'b) t -> 'a Index.t -> 'b IndexSet.t
-val to_list : ('a, 'b) t -> ('a, 'b) piece list
-val map : (('a1, 'b1) piece -> ('a2, 'b2) piece list) -> ('a1, 'b1) t -> ('a2, 'b2) t
-val from : 'a IndexSet.t -> ('a index -> 'b IndexSet.t) -> ('a, 'b) t
+  val minimum : ('a, 'b) t
+  val is_minimum : ('a, 'b) t -> bool
+  val piece : 'a IndexSet.t -> 'b IndexSet.t -> ('a, 'b) t
+  val piecewise : ('a IndexSet.t * 'b IndexSet.t) list -> ('a, 'b) t
+  val increase :
+    ?ignore:'b IndexSet.t ->
+    ('a, 'b) t -> ('a, 'b) t -> ('a, 'b) t * ('a, 'b) t
+  val image : ('a, 'b) t -> 'a Index.t -> 'b IndexSet.t
+  val to_list : ('a, 'b) t -> ('a, 'b) piece list
+  val map : (('a1, 'b1) piece -> ('a2, 'b2) piece) -> ('a1, 'b1) t -> ('a2, 'b2) t
+  val from : 'a IndexSet.t -> ('a index -> 'b IndexSet.t) -> ('a, 'b) t
+end
+
+module Decreasing : sig
+  type ('a, 'b) piece = ('a IndexSet.t * 'b IndexSet.t)
+  type ('a, 'b) t = private ('a, 'b) piece list
+
+  val maximum : ('a, 'b) t
+  val is_maximum : ('a, 'b) t -> bool
+  val piece : 'a IndexSet.t -> 'b IndexSet.t -> ('a, 'b) t
+  val piecewise : ('a IndexSet.t * 'b IndexSet.t) list -> ('a, 'b) t
+  val decrease : ('a, 'b) t -> ('a, 'b) t -> ('a, 'b) t * ('a, 'b) t
+  val image : ('a, 'b) t -> 'a Index.t -> 'b IndexSet.t option
+  val to_list : ('a, 'b) t -> ('a, 'b) piece list
+  val map : (('a1, 'b1) piece -> ('a2, 'b2) piece) -> ('a1, 'b1) t -> ('a2, 'b2) t
+end
