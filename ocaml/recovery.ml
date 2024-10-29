724650c7cd52cc64ff42fb3189d5bd080e82a77c
Frédéric Bour <frederic.bour@lakaban.net>
2024-10-30 07:37:51 +0900

    Recovery: fix minimization

--- ocaml/recovery.ml
+++ ocaml/recovery.ml
@@ -112,76 +112,298 @@ let positive_items st =
 
 let time = Stopwatch.create ()
 
-type config = {
-  goto: Lr1.t;
-  base: Lr1.t;
-  lookaheads: Terminal.set;
-}
+module NFA = struct
+  include Gensym()
 
-let ignore_lookaheads = true
+  type config = {
+    goto: Lr1.t;
+    base: Lr1.t;
+    lookaheads: Terminal.set;
+  }
 
-type state = {
-  config: config;
-  mutable epsilon: state list;
-  mutable transitions: state list list;
-}
+  let ignore_lookaheads = true
 
-let table = Hashtbl.create 7
+  type state = {
+    index: n index;
+    config: config;
+    mutable epsilon: n indexset;
+    mutable transitions: n indexset list;
+  }
 
-let rec explore states la n = function
-  | [] -> []
-  | (it, la') :: xs when Item.position it = n ->
-    let la' =
-      if ignore_lookaheads then Terminal.all else IndexSet.inter la la'
-    in
-    if IndexSet.is_empty la' then
-      explore states la n xs
-    else
-      let x', xs' = match explore states la n xs with
-        | [] -> ([], [])
-        | x' :: xs' -> (x', xs')
+  let table = Hashtbl.create 7
+
+  let rec explore states la n = function
+    | [] -> []
+    | (it, la') :: xs when Item.position it = n ->
+      let la' =
+        if ignore_lookaheads then Terminal.all else IndexSet.inter la la'
       in
-      let p = Item.production it in
-      let nt = Production.lhs p in
-      IndexSet.fold (fun base acc ->
-          let goto = Transition.find_goto_target base nt in
-          let config = {base; goto; lookaheads = la'} in
-          get_state config :: acc
-        ) states x' :: xs'
-  | xs ->
-    [] :: explore (indexset_bind states Lr1.predecessors) la (n + 1) xs
+      if IndexSet.is_empty la' then
+        explore states la n xs
+      else
+        let x', xs' = match explore states la n xs with
+          | [] -> (IndexSet.empty, [])
+          | x' :: xs' -> (x', xs')
+        in
+        let p = Item.production it in
+        let nt = Production.lhs p in
+        IndexSet.fold (fun base acc ->
+            let goto = Transition.find_goto_target base nt in
+            let config = {base; goto; lookaheads = la'} in
+            IndexSet.add (get_state config) acc
+          ) states x' :: xs'
+    | xs ->
+      IndexSet.empty :: explore (indexset_bind states Lr1.predecessors) la (n + 1) xs
 
-and get_state config =
-  match Hashtbl.find_opt table config with
-  | Some st -> st
-  | None ->
-    let st = {config; epsilon=[]; transitions=[]} in
-    Hashtbl.add table config st;
-    begin match
-        explore
-          (IndexSet.singleton st.config.base)
-          config.lookaheads 1
-          (positive_items st.config.goto)
-      with
-      | [] -> ()
-      | eps :: rest ->
-        st.epsilon <- eps;
-        st.transitions <- rest
-    end;
-    st
+  and get_state config =
+    match Hashtbl.find_opt table config with
+    | Some st -> st.index
+    | None ->
+      let st = {
+        index = fresh (); config;
+        epsilon=IndexSet.empty;
+        transitions=[]
+      } in
+      Hashtbl.add table config st;
+      begin match
+          explore
+            (IndexSet.singleton st.config.base)
+            config.lookaheads 1
+            (positive_items st.config.goto)
+        with
+        | [] -> ()
+        | eps :: rest ->
+          st.epsilon <- eps;
+          st.transitions <- rest
+      end;
+      st.index
 
-let initial =
-  let acc = ref [] in
-  Index.iter Lr1.n (fun lr1 ->
-      match Lr1.incoming lr1 with
-      | Some s when Symbol.is_nonterminal s -> ()
-      | _ ->
-        let states = IndexSet.singleton lr1 in
-        let items = positive_items lr1 in
-        push acc (lr1, explore states Terminal.all 0 items)
-    );
-  !acc
+  let initial =
+    let acc = ref [] in
+    Index.iter Lr1.n (fun lr1 ->
+        match Lr1.incoming lr1 with
+        | Some s when Symbol.is_nonterminal s -> ()
+        | _ ->
+          let states = IndexSet.singleton lr1 in
+          let items = positive_items lr1 in
+          push acc (lr1, explore states Terminal.all 0 items)
+      );
+    !acc
 
-let () =
-  Printf.eprintf "Non-deterministic full reduction graph: %d states\n"
-    (Hashtbl.length table)
+  let states = Vector.make' n (fun () ->
+      let dummy_lr1 = Index.of_int Lr1.n 0 in {
+        index = Index.of_int n 0;
+        config = {base = dummy_lr1; goto = dummy_lr1; lookaheads = Terminal.all};
+        epsilon = IndexSet.empty;
+        transitions = [];
+      }
+    )
+
+  let () = Hashtbl.iter
+      (fun _ st -> Vector.set states st.index st) table
+
+  let () =
+    Printf.eprintf "Non-deterministic full reduction graph: %d states\n" (cardinal n)
+end
+
+module DFA = struct
+
+  module Tr_merge : sig
+    type t
+    val empty : t
+    val add : NFA.n indexset list -> t -> t
+    val pop : t -> (NFA.n indexset * t) option
+    val is_empty : t -> bool
+  end = struct
+    let is_empty = function [] -> true | _ -> false
+
+    type t = NFA.n indexset list
+
+    let empty = []
+
+    let rec add xs ys =
+      match xs, ys with
+      | v, [] | [], v -> v
+      | (x :: xs'), (y :: ys') ->
+        IndexSet.union x y :: add xs' ys'
+
+    let pop = function
+      | [] -> None
+      | x :: xs -> Some (x, xs)
+  end
+
+  include Gensym()
+
+  type state = {
+    index: n index;
+    mutable reached: NFA.n indexset;
+    mutable wildcard: state option;
+    mutable transitions: (Lr1.t * state) list;
+  }
+
+  let accumulate_trs ss trs =
+    let rec add_one state (reached, trs) =
+      let state = Vector.get NFA.states state in
+      let trs = Tr_merge.add state.transitions trs in
+      add_many state.epsilon (reached, trs)
+    and add_many states (reached, trs) =
+      let reached' = IndexSet.union states reached in
+      if reached == reached' then
+        (reached, trs)
+      else
+        IndexSet.fold add_one states (reached', trs)
+    in
+    add_many ss (IndexSet.empty, trs)
+
+  let table = Hashtbl.create 7
+
+  let group ss =
+    IndexSet.fold (fun state map ->
+        IndexMap.update (Vector.get NFA.states state).config.base (function
+            | None -> Some (IndexSet.singleton state)
+            | Some states -> Some (IndexSet.add state states)
+          ) map
+      ) ss IndexMap.empty
+
+  let todo = ref []
+
+  let rec follow_transitions (reached, trs as key) =
+    match Hashtbl.find_opt table key with
+    | Some ds -> ds
+    | None ->
+      let ds = {index = fresh(); reached; wildcard = None; transitions = []} in
+      Hashtbl.add table key ds;
+      begin match Tr_merge.pop trs with
+        | None -> ()
+        | Some (states, next) when IndexSet.is_empty states ->
+          assert (Option.is_none ds.wildcard);
+          ds.wildcard <- Some (follow_transitions (IndexSet.empty, next))
+        | Some (states, next) ->
+          IndexMap.iter (fun lr1 states ->
+              push todo (ds, lr1, (accumulate_trs states next))
+            ) (group states)
+      end;
+      ds
+
+  let initial = {
+    index = fresh ();
+    reached = IndexSet.empty;
+    wildcard = None;
+    transitions = List.map (fun (lr1, tr) ->
+        let tr = Tr_merge.add tr Tr_merge.empty in
+        (lr1, follow_transitions (IndexSet.empty, tr))
+      ) NFA.initial;
+  }
+
+  let () =
+    let rec loop = function
+      | [] ->
+        begin match !todo with
+          | [] -> ()
+          | todo' ->
+            todo := [];
+            loop todo'
+        end
+      | (ds, lr1, key) :: xs ->
+        ds.transitions <- (lr1, follow_transitions key) :: ds.transitions;
+        loop xs
+    in
+    loop []
+
+  let states = Vector.make n initial
+
+  let () =
+    Hashtbl.iter (fun _ ds -> Vector.set states ds.index ds) table
+
+  let () =
+    Printf.eprintf "Deterministic full reduction graph: %d states\n"
+      (cardinal n)
+end
+
+module MDFA = struct
+
+  module Label = struct
+    type t = Lr1.t option
+    let compare t1 t2 =
+      match t1, t2 with
+      | None, None -> 0
+      | Some _, None -> +1
+      | None, Some _ -> -1
+      | Some l1, Some l2 -> Index.compare l1 l2
+  end
+
+  module Transitions = struct
+    include IndexBuffer.Gen.Make()
+
+    let table = get_generator ()
+
+    let () =
+      Vector.iter (fun ds ->
+          begin match ds.DFA.wildcard with
+            | None -> ()
+            | Some ds' ->
+              let _ = IndexBuffer.Gen.add table (ds.index, None, ds'.index) in
+              ()
+          end;
+          List.iter (fun (lr1, ds') ->
+              let _ = IndexBuffer.Gen.add table (ds.index, Some lr1, ds'.DFA.index) in
+              ()
+            ) ds.transitions
+        ) DFA.states
+
+    let table = IndexBuffer.Gen.freeze table
+  end
+
+  module Input = struct
+    type states = DFA.n
+    let states = DFA.n
+
+    type transitions = Transitions.n
+    let transitions = Transitions.n
+
+    let label tr =
+      let _, x, _ = Vector.get Transitions.table tr in
+      x
+
+    let source tr =
+      let x, _, _ = Vector.get Transitions.table tr in
+      x
+
+    let target tr =
+      let _, _, x = Vector.get Transitions.table tr in
+      x
+
+    let initials f =
+      f DFA.initial.index
+
+    let finals f =
+      Hashtbl.iter
+        (fun _ ds -> if not (IndexSet.is_empty ds.DFA.reached) then f ds.index)
+        DFA.table
+
+    let refinements f =
+      let by_reached = Hashtbl.create 7 in
+      let get_key ds =
+        IndexSet.map
+          (fun ns -> (Vector.get NFA.states ns).config.goto)
+          ds.DFA.reached
+      in
+      let register ds =
+        if not (IndexSet.is_empty ds.DFA.reached) then
+          let key = get_key ds in
+          match Hashtbl.find_opt by_reached key with
+          | Some r -> push r ds.index
+          | None ->
+            let r = ref [ds.index] in
+            Hashtbl.add by_reached key r
+      in
+      Hashtbl.iter (fun _ ds -> register ds) DFA.table;
+      Hashtbl.iter (fun _ r -> f (fun ~add -> List.iter add !r)) by_reached
+  end
+
+  include Valmari.Minimize(Label)(Input)
+
+  let () =
+    Printf.eprintf "Minimized to: %d states\n" (cardinal states)
+
+end
