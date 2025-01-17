9ce86921b78e8411bb17e7716feab6f78e7d0c60
Frédéric Bour <frederic.bour@lakaban.net>
2025-01-17 10:18:31 +0900

    Comment Info module

--- src/kernel/info.ml
+++ src/kernel/info.ml
@@ -1,3 +1,33 @@
+(* MIT License
+ *
+ * Copyright (c) 2025 Frédéric Bour
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *)
+
+(** This module defines data structures and operations for handling grammar
+  information in a structured way. It includes representations for terminals,
+  non-terminals, productions, and LR states, along with their transitions and
+  reductions. The module is designed to work with Menhir's grammar
+  representation and extends it with additional functionality for
+  convenience. *)
+
 open Utils
 open Misc
 open Fix.Indexing
@@ -119,7 +149,7 @@ module type S = sig
     val accepting : set
 
     (* An LR(1) state is idle if the parser needs to look at more input
-       before knowing how to proceeds.
+       before knowing how to proceed.
        Idle states are the initial states and the targets of SHIFT transitions
        (states with a terminal as incoming symbol), except the accepting ones
        (after reading EOF, the only valid action is to reduce). *)
