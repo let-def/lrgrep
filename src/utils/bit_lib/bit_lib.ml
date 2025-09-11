e26ba96d8236075e4f8968e478c2f22bda630191
Frédéric Bour <frederic.bour@lakaban.net>
2025-09-11 12:11:42 +0900

    Use specialized instructions for bit manipulation

---
+++ src/utils/bit_lib/bit_lib.ml
@@ -0,0 +1,22 @@
+external pop_count : int -> (int [@untagged]) =
+  "bit_lib_pop_count_tagged" "bit_lib_pop_count" [@@noalloc]
+
+external msb_index : int -> (int [@untagged]) =
+  "bit_lib_msb_index_tagged" "bit_lib_msb_index" [@@noalloc]
+
+external msb_index_slow : int -> int =
+  "bit_lib_msb_index_tagged" [@@noalloc]
+
+external lsb_index : int -> (int [@untagged]) =
+  "bit_lib_lsb_index_tagged" "bit_lib_lsb_index" [@@noalloc]
+
+external lsb_index_slow : int -> int =
+  "bit_lib_lsb_index_tagged" [@@noalloc]
+
+external extract_msb : int -> (int [@untagged]) =
+  "bit_lib_extract_msb_tagged" "bit_lib_extract_msb" [@@noalloc]
+
+external extract_msb_slow : int -> int =
+  "bit_lib_extract_msb_tagged" [@@noalloc]
+
+let extract_lsb x = x land -x
